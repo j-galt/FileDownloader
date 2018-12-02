6ea0e6a0e29a7a893108f71029ceb36ad1cd2200
Illia Pivtoraiko <Illia_Pivtoraiko@epam.com>
2018-12-03 00:33:23 +0200

    implemented web files downloader

---
+++ FileDownloader/FileDownloader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebFileDownloader
+{
+    public class FileDownloader
+    {
+        private string _uri;
+        private readonly string _subPageUri = @"(files/Shareholders/([\d]+)/index.html)";
+        private readonly string _fileUri = @"""(.*\.(pdf|PDF))"".*\>[\s]*([\d]+\.[\d]+\.[\d]+)";
+
+        public FileDownloader(string uri)
+        {
+            _uri = uri;
+        }
+
+        public async Task Start()
+        {
+            WebClient wc = new WebClient();
+            await DownloadAllFiles(_uri, @"D:\Test1", wc);
+        }
+
+        private async Task DownloadAllFiles(string uri, string localPath, WebClient wc)
+        {
+            string str;
+
+            try
+            {
+                var data = await wc.OpenReadTaskAsync(new Uri(uri));
+
+                using (StreamReader sr = new StreamReader(data))
+                {
+                    str = await sr.ReadToEndAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            Regex rx = new Regex(_subPageUri, RegexOptions.IgnoreCase);
+            MatchCollection subPageUris = rx.Matches(str);
+
+            foreach (var subPageUri in subPageUris)
+            {
+                string strUri = @"https://bank.gov.ua/" + ((Match)subPageUri).Groups[1].Value;
+                await DownloadAllFiles(strUri, localPath + '/' + ((Match)subPageUri).Groups[2].Value, wc);
+            }
+
+            rx = new Regex(_fileUri, RegexOptions.IgnoreCase);
+            MatchCollection fileUris = rx.Matches(str);
+
+            foreach (var fileUri in fileUris)
+            {
+                string file = ((Match)fileUri).Groups[1].Value;
+                string fileName = ((Match)fileUri).Groups[2].Value;
+                int i = uri.LastIndexOf('/');
+
+                if (i > 0)
+                {
+                    string strUri = uri.Substring(0, i + 1) + file;
+                    await DownloadFileAsync(strUri, localPath, fileName);
+                }
+            }
+        }
+
+        private async Task DownloadFileAsync(string fileUri, string localPath, string fileName) 
+        {
+            bool exist = Directory.Exists(localPath);
+
+            if (!exist)
+            {
+                Directory.CreateDirectory(localPath);
+            }
+            
+            WebClient wc = new WebClient();
+            await wc.DownloadFileTaskAsync(fileUri, localPath + '/' + fileName + ".pdf");        
+        }
+    }
+}
---
+++ FileDownloader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFileDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDownloader fd = new FileDownloader(@"https://bank.gov.ua/control/uk/publish/article?art_id=6738234&cat_id=51342");
+            var awaiter = fd.Start().GetAwaiter();
+
+            awaiter.GetResult();
+        }
+    }
+}
